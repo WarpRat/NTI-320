408f42cb9a4140a39138a7cedf59f96a69b32269
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-04-17 00:01:35 -0700

    Updated nagios server and client script

--- nagios_client_install.sh
+++ nagios_client_install.sh
@@ -2,4 +2,13 @@
 
 apt-get install -y nagios-nrpe-server nagios-plugins
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.138.0.5/g' /etc/nagios/nrpe.cfg
+
+sed -i 's/check_hda1/check_disk/g' /etc/nagios/nrpe.cfg
+sed -i 's/dev\/hda1/dev\/sda1/g' /etc/nagios/nrpe.cfg
+echo "command[check_mem]=/usr/lib/nagios/plugins/check_mem.sh -w 80 -c 90" >> /etc/nagios/nrpe.cfg
+
 systemctl restart nagios-nrpe-server
+
+curl https://raw.githubusercontent.com/WarpRat/NTI-320/master/check_mem.sh >> /usr/lib/nagios/plugins/check_mem.sh
+
+chmod +x /usr/lib/nagios/plugins/check_mem.sh
--- nagios_server_install.sh
+++ nagios_server_install.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+#Install a nagios server
+
 yum -y install nagios
+
 systemctl enable nagios
 systemctl start nagios
-setenforce 0
-yum install -y httpd
+
+#Trying without SELinux in permissive
+#setenforce 0
+
+yum install -y httpd nagios-selinux
+
 systemctl enable httpd
 systemctl restart httpd
+
 yum -y install nrpe
+
 systemctl enable nrpe
 systemctl start nrpe
+
+
+echo '########### NRPE CONFIG LINE #######################
+define command{
+command_name check_nrpe
+command_line $USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
+}' >> /etc/nagios/objects/commands.cfg
+
+systemctl restart nagios
+
