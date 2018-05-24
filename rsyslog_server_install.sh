0f21850bfd142bf4bdec2959cfb3d8b7d172ade6
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-05-23 18:18:49 -0700

    updating scripts for rsyslog and custom repos

--- rsyslog_server_install.sh
+++ rsyslog_server_install.sh
@@ -1,15 +1,17 @@
-!#/bin/bash
-#Very basic bash script that sets up the directory structure needed for
-#a build server
+#!/bin/bash
+#
+#Bash script to set up an rsyslog server
 #
 
-yum install -y rpm-build make gcc git
 
-mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+#Open up TCP and UDP ports to accept logs
+sed -i '/^#$ModLoad imupd/ s/^#/^/g' /etc/rsyslog.conf
+sed -i '/^#$ModLoad imtcp/ s/^#/^/g' /etc/rsyslog.conf
+sed -i '/^#$UDPServerRun/ s/^#/^/g' /etc/rsyslog.conf
+sed -i '/^#$InputTCPServerRun/ s/^#/^/g' /etc/rsyslog.conf
 
-echo '%_topdir %{getenv:HOME}/rpmbuild' > ~/.rpmmacros
-
-#Now ready to compile code and build packages
+#Restart syslog server
+systemctl restart rsyslog
 
 #Install Nagios Monitoring
 yum install -y nrpe nagios-plugins-all
@@ -22,6 +24,8 @@ sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.138.0.3/g' /etc/na
 sed -i 's/check_hda1/check_disk/g' /etc/nagios/nrpe.cfg
 sed -i 's/dev\/hda1/dev\/sda1/g' /etc/nagios/nrpe.cfg
 echo "command[check_mem]=/usr/lib/nagios/plugins/check_mem.sh -w 80 -c 90" >> /etc/nagios/nrpe.cfg
+
+
 #Get the ip address of the first instance with repo in the name - adjust with for loop to add multiple repos at once
 repo_ip=$(gcloud compute instance list | grep repo | sed '/s/\s\{1,\}/ /g' | cut -d ' ' -f 4 | head -n 1)
 
@@ -33,11 +37,6 @@ baseurl=http://$repo_ip/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
 " >> /etc/yum.repos.d/NTI-320.repo
-
-#Syslog
-echo "*.info;mail.none;authpriv.none;cron.none   @10.138.0.4" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
-
-#####CLEAN UP#####
 
 #Get instance name and zone
 name=$(curl -H "Metadata-Flavor:Google" http://metadata.google.internal/computeMetadata/v1/instance/name)
